936c302aaa184d326075d370f62aa6ecf99617e6
Artur Signell <artur@vaadin.com>
2015-10-08 22:15:40 +0300

    Extracted add-on to a separate project

--- src/main/java/org/vaadin/artur/portalpushdemo/PortalPushDemoServlet.java
+++ src/main/java/org/vaadin/artur/portalpushdemo/PortalPushDemoServlet.java
@@ -6,8 +6,7 @@ import org.vaadin.artur.portalpush.PushServlet;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
 
-// Serlvet which handles pushing and serving of static resources
+// Servlet which handles pushing and serving of static resources
 @WebServlet(urlPatterns = "/*", name = "PortalPushDemoServlet", asyncSupported = true)
-@VaadinServletConfiguration(ui = PortalPushDemoUI.class, productionMode = false)
 public class PortalPushDemoServlet extends PushServlet {
 }
